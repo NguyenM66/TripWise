78ec9eedbeb65b60dc8cf7979ec118eeb962359b
Monica <monica.nguyen@gmail.com>
2018-01-23 20:20:01 -0500

    deploy

--- index.js
+++ index.js
@@ -56,6 +56,6 @@ app.use(function(err,req,res,next){
 
 
 // start the server
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log('Server is running on http://localhost:3000 or http://127.0.0.1:3000');
 });
